9b0f92e30f994463ec23d5ef3d8c0c69285d7438
gmzi <gmzieres@gmail.com>
2023-03-06 17:57:04 -0500

    draft image render on index.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 const removeImports = require('next-remove-imports')();
 
-module.exports = removeImports({})
+// module.exports = removeImports({});
 
-// module.exports = {
-//   reactStrictMode: true,
-// }
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+};
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,37 @@
-import Link from 'next/link'
-import Date from '../components/date'
-import Layout from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { connectToDatabase } from '../lib/mongodb'
-import { data, text } from '../lib/data'
+import Link from 'next/link';
+import Date from '../components/date';
+import Layout from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { connectToDatabase } from '../lib/mongodb';
+import { data, text } from '../lib/data';
+import Image from 'next/image';
 
 const MONGODB_COLLECTION = process.env.MONGODB_COLLECTION;
 
 export default function Home({ posts }) {
-
   return (
     <Layout home>
       <section>
-        <h1 style={{display: "none"}}>{data.title}</h1>
         {!posts.length ? (
           <p>No posts yet...</p>
         ) : (
-          <ul className={utilStyles.list}>
-            {posts.map(({ _id, fileName, title, author, date }) => (
-              <li className={utilStyles.listItem} key={_id}>
-                <div className={utilStyles.titleContainer}>
-                  <Link href={`/posts/${fileName}`}>
-                    <a><span>{title}</span> <span className="postDate">{text.index.by} {author}</span></a>
+          <ul className={utilStyles.cardList}>
+            {posts.map(({ _id, fileName, title, author, date, image_url }) => (
+              <li className={utilStyles.cardListItem} key={_id}>
+                <div className={utilStyles.cardContainer}>
+                  <Link href={`/posts/${fileName}`} passHref>
+                    <div className={utilStyles.card}>
+                      {/* <div className={utilStyles.imgContainer}>
+                        <img src={image_url} alt="test" />
+                      </div> */}
+                      <Image width={500} height={500} src={image_url} />
+                      <a>
+                        <span>{title}</span>{' '}
+                        <span className="postDate">
+                          {text.index.by} {author}
+                        </span>
+                      </a>
+                    </div>
                   </Link>
                 </div>
               </li>
@@ -30,14 +40,14 @@ export default function Home({ posts }) {
         )}
       </section>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps() {
   const { db } = await connectToDatabase();
-  
-  const query = {}
-  const sort = { date: -1, fileName: 1 }
+
+  const query = {};
+  const sort = { date: -1, fileName: 1 };
 
   const posts = await db
     .collection(MONGODB_COLLECTION)
@@ -47,9 +57,9 @@ export async function getStaticProps() {
 
   return {
     props: {
-      posts: JSON.parse(JSON.stringify(posts))
-    }
-  }
+      posts: JSON.parse(JSON.stringify(posts)),
+    },
+  };
 }
 
 /*
