a5fd1c7e4ec9104bedcb1c8def061922a74a1aaa
gmzi <gmzieres@gmail.com>
2023-04-27 14:14:57 -0400

    style metadata form

--- components/forms/metadataForm.js
+++ components/forms/metadataForm.js
@@ -9,12 +9,14 @@ export default function MetadataForm({handleChange, authorName, description}){
     }
 
     return(
-            <form>
-                <h4>Metadata</h4>
+        <div className={"metadataContainer"}>
+            <h4>Metadata</h4>
+            <form className={"metadataForm"}>
                 <label htmlFor="author">{text.addPostForm.authorName}</label>
                 <input type="text" name="author" placeholder={text.addPostForm.authorPlaceholder} value={authorName} onChange={handleFormChange} />
                 <label htmlFor="description">{text.addPostForm.description}</label>
                 <textarea id="description" name="description" placeholder={`(${text.addPostForm.optional})`} value={description} onChange={handleFormChange} />
             </form>
+        </div>
     )
 }
